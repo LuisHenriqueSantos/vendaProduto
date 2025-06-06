9073db635f03b5521f12cafe2e8c7237c0fdae9d
Luís Henrique <78195873+LuisHenriqueSantos@users.noreply.github.com>
2025-06-06 18:50:11 -0300

    Update ProdutoController.java

--- vendaproduto/src/main/java/com/mentorama/vendaproduto/controllers/ProdutoController.java
+++ vendaproduto/src/main/java/com/mentorama/vendaproduto/controllers/ProdutoController.java
@@ -21,6 +21,11 @@ public class ProdutoController {
     public List<Produto> findAll(@RequestParam(required = false) String produto){
       return produtoService.findAll(produto);    
     }
+    
+    @GetMapping("/{id}")
+    public Produto findById(@PathVariable("id") Integer id){
+        return produtoService.findById(id);
+    }
 
     @PostMapping
     public ResponseEntity<Integer> add(@RequestBody final Produto produto){
