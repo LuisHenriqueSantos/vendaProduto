6fae97a4bd1a08493cb0ff522aaa8f0977166781
Luís Henrique <78195873+LuisHenriqueSantos@users.noreply.github.com>
2025-06-06 16:50:30 -0300

    Iniciando projeto

---
+++ vendaproduto/src/main/java/com/mentorama/vendaproduto/VendaprodutoApplication.java
@@ -0,0 +1,13 @@
+package com.mentorama.vendaproduto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendaprodutoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VendaprodutoApplication.class, args);
+    }
+
+}
